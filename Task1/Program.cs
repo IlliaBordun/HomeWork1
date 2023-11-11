7283658daf3e5826011b6ddac79442e1389ec810
Illia_Bordun <nudrob555@gmail.com>
2023-11-11 11:47:08 +0200

    Добавьте файлы проекта.

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int a))
+            {
+                Console.WriteLine($"Area: {a * a}");
+                Console.WriteLine($"Perimeter: {a * 4}");
+            }
+            else
+            {
+                Console.WriteLine("Please, enter an integer value");
+            }
+        }
+    }
+}
---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            if (double.TryParse(input, out double r))
+            {
+                double pi = 3.14;
+                double lengthValue = 2 * pi * r;
+                double areaValue = pi * r * r;
+                double volumeValue = 4 / 3 * pi * r * r * r;
+                Console.Write($"length: {lengthValue} \narea: {areaValue} \nvolume: {volumeValue}");
+            }
+            else
+            {
+                Console.WriteLine("Please, enter an integer value");
+            }
+        }
+    }
+}
